cc66651465b67651c9d7502714e00c60025933f7
agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-12-16 16:23:52 +0000

    EPBDS: added project for mapping feature

---
+++ MAPPING/trunk/org.openl.rules.mapping.dev.modified.test/src/test/java/org/openl/rules/mapping/MapperTest.java
@@ -0,0 +1,12 @@
+package org.openl.rules.mapping;
+
+import org.dozer.DozerBeanMapper;
+import org.junit.Test;
+
+public class MapperTest {
+
+    @Test
+    public void test1() {
+        //empty test
+    }
+}
---
+++ MAPPING/trunk/org.openl.rules.mapping.dev.modified/src/main/java/org/dozer/DozerBeanMapper.java
@@ -0,0 +1,19 @@
+package org.dozer;
+
+public class DozerBeanMapper implements Mapper {
+
+    public <T> T map(Object arg0, Class<T> arg1, String arg2) throws MappingException {
+        return null;
+    }
+
+    public <T> T map(Object arg0, Class<T> arg1) throws MappingException {
+        return null;
+    }
+
+    public void map(Object arg0, Object arg1, String arg2) throws MappingException {
+    }        
+
+    public void map(Object arg0, Object arg1) throws MappingException {
+    }
+    
+}
