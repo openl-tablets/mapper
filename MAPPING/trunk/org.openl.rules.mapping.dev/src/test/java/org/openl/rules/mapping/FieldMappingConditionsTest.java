b76d7b0dd1793df5c44cabbfd8618dd370837394
agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-01-20 14:06:53 +0000

    EPBDS: added unit test for conditions feature.

---
+++ MAPPING/trunk/org.openl.rules.mapping.dev/src/test/java/org/openl/rules/mapping/FieldMappingConditionsTest.java
@@ -0,0 +1,34 @@
+package org.openl.rules.mapping;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.openl.rules.mapping.to.A;
+import org.openl.rules.mapping.to.C;
+
+public class FieldMappingConditionsTest {
+
+    @Test
+    public void indexedListsWithoutGenericTypeTest() {
+
+        File source = new File("src/test/resources/org/openl/rules/mapping/FieldMappingConditionsTest.xlsx");
+        RulesBeanMapper mapper = RulesBeanMapperFactory.createMapperInstance(source);
+
+        A a = new A();
+        a.setAString("a-string");
+        a.setAnInteger(100);
+
+        C c = mapper.map(a, C.class);
+        assertEquals("a-string", c.getAString());
+        assertEquals("a-string", c.getB().getAString());
+        assertEquals(Integer.valueOf(100), c.getB().getAnInteger());
+
+        A a1 = mapper.map(c, A.class);
+        
+        assertEquals("a-string", a1.getAString());
+        assertEquals(null, a1.getAnInteger());
+    }
+
+}
