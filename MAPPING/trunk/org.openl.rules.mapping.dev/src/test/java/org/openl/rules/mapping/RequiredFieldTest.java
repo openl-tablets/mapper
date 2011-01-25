14db9ae3f12908af3d6134e8b9cb131e476cc58c
agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-01-25 14:21:01 +0000

    EPBDS: added unit test for fields mapping when destination one is required.

---
+++ MAPPING/trunk/org.openl.rules.mapping.dev/src/test/java/org/openl/rules/mapping/RequiredFieldTest.java
@@ -0,0 +1,37 @@
+package org.openl.rules.mapping;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.openl.rules.mapping.to.A;
+import org.openl.rules.mapping.to.B;
+
+public class RequiredFieldTest {
+
+    @Test
+    public void requiredClauseIsPassedTest() {
+
+        File source = new File("src/test/resources/org/openl/rules/mapping/required/RequiredFieldTest.xlsx");
+        RulesBeanMapper mapper = RulesBeanMapperFactory.createMapperInstance(source);
+
+        A a = new A();
+        a.setAString("a-string");
+
+        B b = mapper.map(a, B.class);
+
+        assertEquals("a-string", b.getAString());
+    }
+
+    @Test(expected = RulesMappingException.class)
+    public void requiredClauseIsNotPassedTest() {
+
+        File source = new File("src/test/resources/org/openl/rules/mapping/required/RequiredFieldTest.xlsx");
+        RulesBeanMapper mapper = RulesBeanMapperFactory.createMapperInstance(source);
+
+        A a = new A();
+        mapper.map(a, B.class);
+    }
+
+}
