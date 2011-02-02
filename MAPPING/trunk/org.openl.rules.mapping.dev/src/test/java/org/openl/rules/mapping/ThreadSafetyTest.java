84fe64346c8fc99fceb1eeec15f426da3bb6075b
agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-02-02 17:11:10 +0000

    EPBDS: reduced threads count for unit test

--- MAPPING/trunk/org.openl.rules.mapping.dev/src/test/java/org/openl/rules/mapping/ThreadSafetyTest.java
+++ MAPPING/trunk/org.openl.rules.mapping.dev/src/test/java/org/openl/rules/mapping/ThreadSafetyTest.java
@@ -15,7 +15,7 @@ public class ThreadSafetyTest {
     @Test
     public void threadSafetyTest() {
 
-        int threadsCount = 100;
+        int threadsCount = 5;
         final ExecutionStatus status = new ExecutionStatus();
 
         File source = new File("src/test/resources/org/openl/rules/mapping/thread/ThreadSafetyTest.xlsx");
