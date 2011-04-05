e24dddcb8f97a7c25967245e72384f6e7bc46104
agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-04-05 13:55:37 +0000

    EPBDS: added tests for base implementations of mapping aware custom converters and field mapping conditions.

--- MAPPING/trunk/org.openl.rules.mapping.dev/src/test/java/org/openl/rules/mapping/ThreadSafetyTest.java
+++ MAPPING/trunk/org.openl.rules.mapping.dev/src/test/java/org/openl/rules/mapping/ThreadSafetyTest.java
@@ -1,6 +1,7 @@
 package org.openl.rules.mapping;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -8,14 +9,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.dozer.BaseMappingParamsAwareCustomConverter;
 import org.dozer.BaseMappingParamsAwareFieldMappingCondition;
+import org.dozer.CustomConverter;
 import org.dozer.FieldMappingCondition;
 import org.dozer.MappingContext;
 import org.dozer.MappingParameters;
 import org.junit.Test;
 import org.openl.rules.mapping.to.A;
 import org.openl.rules.mapping.to.B;
-import org.openl.rules.mapping.to.C;
 
 public class ThreadSafetyTest {
 
@@ -60,7 +62,7 @@ public class ThreadSafetyTest {
         assertTrue(status.getExceptions().isEmpty());
     }
     
-//    @Test
+    @Test
     public void fieldMapConditionWithIdSupportTest() {
 
         Map<String, FieldMappingCondition> conditions = new HashMap<String, FieldMappingCondition>();
@@ -92,11 +94,6 @@ public class ThreadSafetyTest {
         final A a = new A();
         a.setAString("a-string");
 
-        MappingContext context2 = new MappingContext();
-        MappingParameters params2 = new MappingParameters();
-        params2.put("mapField", false);
-        context2.setParams(params2);
-        
         Thread thread1 = new Thread(new Runnable() {
             public void run() {
                 MappingContext context1 = new MappingContext();
@@ -115,14 +112,111 @@ public class ThreadSafetyTest {
             }
         });
         
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                MappingContext context2 = new MappingContext();
+                MappingParameters params2 = new MappingParameters();
+                params2.put("mapField", false);
+                context2.setParams(params2);
+                
+                try {
+                    B b2 = mapper.map(a, B.class, context2);
+                    assertEquals(null, b2.getAString());
+                } catch (Exception e) {
+                    status.getExceptions().add(e);
+                } finally {
+                    status.setCounter(status.getCounter() + 1);
+                }
+            }
+        });
+
         thread1.start();
-
-
-
-        B b2 = mapper.map(a, B.class, context2);
-        assertEquals(null, b2.getAString());
+        thread2.start();
+        
+        while (status.getCounter() < 2) {
+            // wait until all threads are terminated
+        }
+        
+        assertTrue(status.getExceptions().isEmpty());
     }
 
+    @Test
+    public void convertersWithIdSupportTest() {
+
+        Map<String, CustomConverter> convertersMap = new HashMap<String, CustomConverter>();
+        convertersMap.put("convert", new BaseMappingParamsAwareCustomConverter() {
+            
+            @Override
+            public Object convert(MappingParameters params, Object existingDestinationFieldValue,
+                Object sourceFieldValue, Class<?> destinationClass, Class<?> sourceClass) {
+                return params.get("value");
+            }
+            
+            @Override
+            public void setMappingParams(MappingParameters params) {
+                super.setMappingParams(params);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        File source = new File(
+            "src/test/resources/org/openl/rules/mapping/thread/MappingParamsAwareCustomConverterThreadSafetyTest.xlsx");
+        final Mapper mapper = RulesBeanMapperFactory.createMapperInstance(source, convertersMap, null);
+
+        final ExecutionStatus status = new ExecutionStatus();
+        
+        final A a = new A();
+        a.setAString("a-string");
+
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                MappingContext context1 = new MappingContext();
+                MappingParameters params1 = new MappingParameters();
+                params1.put("value", "value1");
+                context1.setParams(params1);
+                
+                try {
+                    B b1 = mapper.map(a, B.class, context1);
+                    assertEquals("value1", b1.getAString());
+                } catch (Exception e) {
+                    status.getExceptions().add(e);
+                } finally {
+                    status.setCounter(status.getCounter() + 1);
+                }
+            }
+        });
+        
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                MappingContext context2 = new MappingContext();
+                MappingParameters params2 = new MappingParameters();
+                params2.put("value", "value2");
+                context2.setParams(params2);
+                
+                try {
+                    B b2 = mapper.map(a, B.class, context2);
+                    assertEquals("value2", b2.getAString());
+                } catch (Exception e) {
+                    status.getExceptions().add(e);
+                } finally {
+                    status.setCounter(status.getCounter() + 1);
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        
+        while (status.getCounter() < 2) {
+            // wait until all threads are terminated
+        }
+        
+        assertTrue(status.getExceptions().isEmpty());
+    }
     
     private static class ExecutionStatus {
         private int counter;
