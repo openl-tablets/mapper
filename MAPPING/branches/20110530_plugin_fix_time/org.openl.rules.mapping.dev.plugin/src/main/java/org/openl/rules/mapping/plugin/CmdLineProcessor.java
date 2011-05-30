16ba6213ce8c2eb1787487a26c072b10f5551333
iholub@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <iholub@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-05-30 14:17:38 +0000

    add additonal parameters to plugin command line tool to allow usage of previously generated types xml file

---
+++ MAPPING/branches/20110530_plugin_fix_time/org.openl.rules.mapping.dev.plugin/src/main/java/org/openl/rules/mapping/plugin/CmdLineProcessor.java
@@ -0,0 +1,165 @@
+package org.openl.rules.mapping.plugin;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Class that encapsulate command line processing.
+ */
+class CmdLineProcessor {
+
+    private static final Log LOG = LogFactory.getLog(CmdLineProcessor.class);
+
+    private static final String JAR_PATH_OPTION_LONG_NAME = "jar-path";
+    private static final String JAR_PATH_OPTION_NAME = "jp";
+
+    private static final String OUTPUT_XML_PATH_OPTION_LONG_NAME = "output-xml";
+    private static final String OUTPUT_XML_PATH_OPTION_NAME = "o";
+
+    private static final String TYPE_EXPORT_OPTION_LONG_NAME = "export-types";
+    private static final String TYPE_EXPORT_OPTION_NAME = "t";
+
+    private static final String TYPES_XML_PATH_OPTION_LONG_NAME = "types-xml";
+    private static final String TYPES_XML_PATH_OPTION_NAME = "tx";
+
+    private static final String MSG_EXPORT_OPTION_LONG_NAME = "export-messages";
+    private static final String MSG_EXPORT_OPTION_NAME = "m";
+
+    private static final String OMIT_LOG_ERRORS_OPTION_LONG_NAME = "omit-log-errors";
+    private static final String OMIT_LOG_ERRORS_OPTION_NAME = "ole";
+
+    private static final String GENERATE_TYPES_OPTION_LONG_NAME = "generate-types";
+    private static final String GENERATE_TYPES_OPTION_NAME = "g";
+
+    private static final String HELP_OPTION_LONG_NAME = "help";
+    private static final String HELP_OPTION_NAME = "h";
+
+    private Options cmdOptions;
+
+    /**
+     * Parses command line parameters.
+     * 
+     * @param args command line parameters
+     * @return command line arguments and options
+     */
+    public CmdLineArgs parse(String[] args) {
+
+        CmdLineArgs cmdArgs = new CmdLineArgs();
+
+        // Process command line arguments
+        try {
+            CommandLineParser cmdLineParser = new GnuParser();
+            Options cmdOptions = getCmdOptions();
+
+            CommandLine commandLine = cmdLineParser.parse(cmdOptions, args);
+
+            if (commandLine.hasOption(JAR_PATH_OPTION_NAME)) {
+                cmdArgs.setJarpath(commandLine.getOptionValue(JAR_PATH_OPTION_NAME));
+            }
+            if (commandLine.hasOption(OUTPUT_XML_PATH_OPTION_NAME)) {
+                cmdArgs.setOutpath(commandLine.getOptionValue(OUTPUT_XML_PATH_OPTION_NAME));
+            }
+            if (commandLine.hasOption(TYPES_XML_PATH_OPTION_NAME)) {
+                cmdArgs.setTypesXmlPath(commandLine.getOptionValue(TYPES_XML_PATH_OPTION_NAME));
+            }
+
+            cmdArgs.setHelp(commandLine.hasOption(HELP_OPTION_NAME));
+            cmdArgs.setExportTypes(commandLine.hasOption(TYPE_EXPORT_OPTION_NAME));
+            cmdArgs.setExportMessages(commandLine.hasOption(MSG_EXPORT_OPTION_NAME));
+            cmdArgs.setOmitLogErrors(commandLine.hasOption(OMIT_LOG_ERRORS_OPTION_NAME));
+            cmdArgs.setGenerateTypes(commandLine.hasOption(GENERATE_TYPES_OPTION_NAME));
+
+            if (commandLine.getArgs().length == 0) {
+                throw new RuntimeException("Source file is not defined");
+            }
+
+            if (commandLine.getArgs().length > 1) {
+                throw new RuntimeException("Cannot resolve source file");
+            }
+
+            cmdArgs.setSourcepath(commandLine.getArgs()[0]);
+        } catch (ParseException parseException) {
+            LOG.error("Encountered exception while parsing command line arguments", parseException);
+        }
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug(cmdArgs.toString());
+        }
+
+        return cmdArgs;
+    }
+
+    @SuppressWarnings("static-access")
+    private Options getCmdOptions() {
+
+        if (cmdOptions == null) {
+            Option out = OptionBuilder.withDescription("output xml file path")
+                .withArgName("path")
+                .hasArg()
+                .withLongOpt(OUTPUT_XML_PATH_OPTION_LONG_NAME)
+                .create(OUTPUT_XML_PATH_OPTION_NAME);
+
+            Option jarpath = OptionBuilder.withDescription("folders list which will be scanned to find jar files")
+                .withArgName("path")
+                .hasArg()
+                .withLongOpt(JAR_PATH_OPTION_LONG_NAME)
+                .create(JAR_PATH_OPTION_NAME);
+
+            Option help = OptionBuilder.withDescription("print current message")
+                .withLongOpt(HELP_OPTION_LONG_NAME)
+                .create(HELP_OPTION_NAME);
+
+            Option exportTypes = OptionBuilder.withDescription("export types")
+                .withLongOpt(TYPE_EXPORT_OPTION_LONG_NAME)
+                .create(TYPE_EXPORT_OPTION_NAME);
+
+            Option typesXmlPath = OptionBuilder.withDescription("types xml file path")
+                .withArgName("path")
+                .hasArg()
+                .withLongOpt(TYPES_XML_PATH_OPTION_LONG_NAME)
+                .create(TYPES_XML_PATH_OPTION_NAME);
+
+            Option exportMessages = OptionBuilder.withDescription("export errors and warinings")
+                .withLongOpt(MSG_EXPORT_OPTION_LONG_NAME)
+                .create(MSG_EXPORT_OPTION_NAME);
+
+            Option omitLogErrors = OptionBuilder.withDescription("omit log errors")
+                .withLongOpt(OMIT_LOG_ERRORS_OPTION_LONG_NAME)
+                .create(OMIT_LOG_ERRORS_OPTION_NAME);
+
+            Option generateTypesXml = OptionBuilder.withDescription("generate types xml")
+                .withLongOpt(GENERATE_TYPES_OPTION_LONG_NAME)
+                .create(GENERATE_TYPES_OPTION_NAME);
+
+            cmdOptions = new Options();
+            cmdOptions.addOption(out);
+            cmdOptions.addOption(jarpath);
+            cmdOptions.addOption(exportTypes);
+            cmdOptions.addOption(typesXmlPath);
+            cmdOptions.addOption(exportMessages);
+            cmdOptions.addOption(omitLogErrors);
+            cmdOptions.addOption(generateTypesXml);
+            cmdOptions.addOption(help);
+        }
+
+        return cmdOptions;
+    }
+
+    /**
+     * Prints usage topic.
+     */
+    public void printUsage() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java -jar org.openl.rules.mapping.dev.plugin-<version>.jar <openl_project_source> [options]",
+            getCmdOptions());
+    }
+
+}
---
+++ MAPPING/branches/20110530_plugin_fix_time/org.openl.rules.mapping.dev.plugin/src/main/java/org/openl/rules/mapping/plugin/serialize/ClassSerializer.java
@@ -0,0 +1,109 @@
+package org.openl.rules.mapping.plugin.serialize;
+
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.dozer.util.CollectionUtils;
+import org.dozer.util.ReflectionUtils;
+
+/**
+ * Serializes {@link Class} objects into internal domain model.
+ * 
+ * Intended for internal use.
+ */
+public class ClassSerializer {
+
+    private static final Log LOG = LogFactory.getLog(ClassSerializer.class); 
+   
+    private static final String[] ignoredProperties = new String [] {"class"};
+    
+    public static BeanEntry serialize(Class<?> clazz, boolean omitLogErrors) {
+        if (clazz == null) {
+            return null;
+        }
+
+        BeanEntry bean = new BeanEntry();
+        bean.setName(clazz.getName());
+        bean.setExtendedType(getSuperclass(clazz));
+
+        PropertyDescriptor[] propDescriptors = null;
+        try {
+            propDescriptors = ReflectionUtils.getPropertyDescriptors(clazz);
+        } catch (Throwable e) {
+            if (!omitLogErrors) {
+                LOG.error(String.format("An error has occured while loading properties of class '%s'", clazz.getName()), e);
+            }
+        }
+        
+        if (propDescriptors == null) {
+            return bean;
+        }
+        
+        List<FieldEntry> fields = new ArrayList<FieldEntry>(propDescriptors.length);
+
+        for (PropertyDescriptor propDescriptor : propDescriptors) {
+            if (!isIgnoredProperty(propDescriptor.getName())) {
+                try {
+                    Class<?> propertyType = propDescriptor.getPropertyType();
+                    
+                    if (propertyType != null) {
+                        FieldEntry field = new FieldEntry();
+                        field.setName(propDescriptor.getName());
+                        field.setType(propertyType);
+
+                        boolean isCollection = CollectionUtils.isCollection(propertyType);
+                        boolean isArray = CollectionUtils.isArray(propertyType);
+                        if (isCollection || isArray) {
+                            field.setCollectionType(isArray ? CollectionType.ARRAY : CollectionType.COLLECTION);
+                            field.setCollectionItemType(ReflectionUtils.getComponentType(propertyType,
+                                propDescriptor,
+                                Object.class));
+                        }
+
+                        fields.add(field);
+                    }
+                } catch (Throwable e) {
+                    if (!omitLogErrors) {
+                        LOG.error(String.format("An error has occurred while processing property '%s' of class %s", propDescriptor.getName(), clazz.getName()), e);
+                    }
+                }
+            }
+        }
+
+        bean.setFields(fields);
+
+        return bean;
+    }
+
+    public static List<BeanEntry> serialize(List<Class<?>> classes, boolean omitLogErrors) {
+        List<BeanEntry> beans = new ArrayList<BeanEntry>(classes.size());
+
+        for (Class<?> clazz : classes) {
+            beans.add(serialize(clazz, omitLogErrors));
+        }
+
+        return beans;
+    }
+    
+    private static Class<?> getSuperclass(Class<?> clazz) {
+        Class<?> superclass = clazz.getSuperclass();
+        if (Object.class == superclass) {
+            return null;
+        }
+        
+        return superclass;
+    }
+    
+    private static boolean isIgnoredProperty(String propName) {
+        for (String ignoredProperty : ignoredProperties) {
+            if (ignoredProperty.equals(propName)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
